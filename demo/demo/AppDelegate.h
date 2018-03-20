9a007314e60cddbc78604fedebdca02e8d34f13d
fuyali <fuyali@panda.tv>
2018-03-20 16:30:43 +0800

    创建项目-demo

---
+++ demo/demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  demo
+//
+//  Created by 692694696@qq.com on 2018/3/20.
+//  Copyright © 2018年 692694696@qq.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ demo/demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  demo
+//
+//  Created by 692694696@qq.com on 2018/3/20.
+//  Copyright © 2018年 692694696@qq.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
